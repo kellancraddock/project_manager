55844eec716bdb8b24c7938378f38b367b483b4c
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-17 22:03:40 -0700

    added renamed file...

--- app/controllers/projectController.php
+++ app/controllers/projectController.php
@@ -1,9 +1,17 @@
 <?php
+	require_once('../app/models/ProjectModel.php');
+	require_once('../app/models/ToolModel.php');
+	require_once('../app/models/LanguageModel.php');
+	require_once('../app/models/AuthorModel.php');
+	
 	class ProjectController extends Zend_Controller_Action
 	{
 		public function init() {
 			//$this->session_alert = new Zend_Session_Namespace('');
-			//$this->Model = new Model();
+			$this->project_model = new ProjectModel();
+			$this->tool_model = new ToolModel();
+			$this->language_model = new LanguageModel();
+			$this->author_model = new AuthorModel();
 			//$this->_helper->layout->setLayout('');
 		}
 		
@@ -24,58 +32,32 @@
 		
 		public function addoneAction() 
 		{
-			$this->view->project_title = $_POST['projectTitle'];
-			$this->view->url = $_POST['projectUrl'];
-			$this->view->author_first = $_POST['authorFirstName'];
-			$this->view->author_last = $_POST['authorLastName'];
-			$this->view->class_name = $_POST['class'];
-			$this->view->complete_date = $_POST['dateComplete'];
+			$tools_helper = $this->_helper->Tools;
+			$tools = $tools_helper->getTools();
 			
-			if (isset($_POST['photoshop'])) {
-				$this->view->photoshop = $_POST['photoshop'];
-			}
-			if (isset($_POST['illustrator'])) {
-				$this->view->illustrator = $_POST['illustrator'];
-			}
-			if (isset($_POST['flash'])) {
-				$this->view->flash = $_POST['flash'];
-			}
-			if (isset($_POST['flex'])) {
-				$this->view->flex = $_POST['flex'];		
-			}
-			if (isset($_POST['air'])) {
-				$this->view->air = $_POST['air']; 
-			}												  	
-			if (isset($_POST['html'])) {
-				$this->view->html = $_POST['html'];		
-			}
-			if (isset($_POST['css'])) {
-				$this->view->css = 	$_POST['css'];	
-			}
-			if (isset($_POST['javascript'])) {
-				$this->view->javascript = $_POST['javascript'];
-			}
-			if (isset($_POST['jquery'])) {
-				$this->view->jquery = $_POST['jquery'];
-			}
-			if (isset($_POST['php'])) {
-				$this->view->php = 	$_POST['php'];
-			}												  	
-			if (isset($_POST['ror'])) {
-				$this->view->ror = 	$_POST['ror'];
-			}
-			if (isset($_POST['as2'])) {
-				$this->view->as2 = 	$_POST['as2'];
-			}
-			if (isset($_POST['as3'])) {
-				$this->view->as3 = 	$_POST['as3'];
-			}
-			if (isset($_POST['sql'])) {
-				$this->view->sql = $_POST['sql'];
+			$languages_helper = $this->_helper->Languages;
+			$languages = $languages_helper->getLanguages();
+			
+			$projects = array($_POST['projectTitle'], $_POST['projectUrl'], $_POST['class'], $_POST['dateComplete'], $_POST['assigmentSpecs'], $_POST['projectApproach']);
+			
+			$authors = array($_POST['authorFirstName'], $_POST['authorLastName']);
+			
+			$project_id = $this->project_model->addOne($projects);
+			
+			$this->author_model->addOne(array($project_id), $authors);
+			
+			foreach (array($tools) as $tool) {
+				$this->tool_model->addOne(array($project_id), $tool);
 			}
 			
-			$this->view->assign = $_POST['assigmentSpecs'];
-			$this->view->project = $_POST['projectApproach'];
+			foreach (array($languages) as $language) {
+				$this->language_model->addOne(array($project_id), $language);
+			}
+			//Go back to index
+			header("Location: /");
+			//Pass project id to view
+			//$this->view->project_id = $project_id;
+			
 		}
 		
 		public function photosAction() 
